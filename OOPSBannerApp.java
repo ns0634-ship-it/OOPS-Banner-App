6e8d53fd4b20de5075fe1ad62cf7e0395dc6023f
ns0634-ship-it <ns0634@srmist.edu.in>
2026-03-03 12:01:24 +0530

    UC1-print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ *
+ * Displays OOPS to the console.
+ *
+ * @author Vidushi
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
