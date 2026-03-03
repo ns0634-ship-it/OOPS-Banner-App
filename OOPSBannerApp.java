d751755a3fddac64afe588ebe07d6728e184259a
ns0634-ship-it <ns0634@srmist.edu.in>
2026-03-03 13:22:09 +0530

    UC3/Implemented OOPS banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,56 @@
 /**
- * OOPSBannerApp UC1 – OOPS Banner Display Application
- *
- * Displays OOPS to the console.
- *
+ * UC3 - Render OOPS as Banner using String.join()
+ * Refactored from UC2 to improve memory efficiency
+ * by replacing + operator with String.join()
+ * 
  * @author Netra
- * @version 1.0
+ * @version 3.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "  ****** ",
+                "  ****** "));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *       "));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *       "));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                "  *****  "));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                "       * "));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                " *     * "));
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " *       ",
+                "  *****  "));
     }
 }
