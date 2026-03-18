b6732bff74b6720562a23fa2cd87a1740d75cbb0
ns0634-ship-it <ns0634@srmist.edu.in>
2026-03-18 10:33:27 +0530

    Completed UC6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,64 +1,60 @@
-/**
- * UC5 - Render OOPS as Banner using Inline Array Initialization
- * Combines declaration and initialization in one statement
- * 
- * @author Netra
- * @version 5.0
- */
-
-public class OOPSBannerApp {
+public class BannerApp {
 
     public static void main(String[] args) {
 
-        // Inline declaration + initialization
+        // Build banner using helper methods
         String[] banner = {
-
-            String.join(" ",
-                    "  *****  ",
-                    "  *****  ",
-                    "  ****** ",
-                    "  ****** "),
-
-            String.join(" ",
-                    " *     * ",
-                    " *     * ",
-                    " *     * ",
-                    " *       "),
-
-            String.join(" ",
-                    " *     * ",
-                    " *     * ",
-                    " *     * ",
-                    " *       "),
-
-            String.join(" ",
-                    " *     * ",
-                    " *     * ",
-                    " ******  ",
-                    "  *****  "),
-
-            String.join(" ",
-                    " *     * ",
-                    " *     * ",
-                    " *       ",
-                    "       * "),
-
-            String.join(" ",
-                    " *     * ",
-                    " *     * ",
-                    " *       ",
-                    " *     * "),
-
-            String.join(" ",
-                    "  *****  ",
-                    "  *****  ",
-                    " *       ",
-                    "  *****  ")
+                String.join("   ", getOPattern()[0], getOPattern()[0], getPPattern()[0], getSPattern()[0]),
+                String.join("   ", getOPattern()[1], getOPattern()[1], getPPattern()[1], getSPattern()[1]),
+                String.join("   ", getOPattern()[2], getOPattern()[2], getPPattern()[2], getSPattern()[2]),
+                String.join("   ", getOPattern()[3], getOPattern()[3], getPPattern()[3], getSPattern()[3]),
+                String.join("   ", getOPattern()[4], getOPattern()[4], getPPattern()[4], getSPattern()[4]),
+                String.join("   ", getOPattern()[5], getOPattern()[5], getPPattern()[5], getSPattern()[5]),
+                String.join("   ", getOPattern()[6], getOPattern()[6], getPPattern()[6], getSPattern()[6])
         };
 
-        // Enhanced for-loop to print banner
+        // Print banner
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Helper method for letter O
+    public static String[] getOPattern() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    // Helper method for letter P
+    public static String[] getPPattern() {
+        return new String[]{
+                "*******  ",
+                "*      * ",
+                "*      * ",
+                "*******  ",
+                "*        ",
+                "*        ",
+                "*        "
+        };
+    }
+
+    // Helper method for letter S
+    public static String[] getSPattern() {
+        return new String[]{
+                " *****   ",
+                "*     *  ",
+                "*        ",
+                " *****   ",
+                "      *  ",
+                "*     *  ",
+                " *****   "
+        };
     }
 }
